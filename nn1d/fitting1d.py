b2401c8d8543e494459860e61b72df31ac22a105
Jinsoo Cho <jinsoo020509@utexas.edu>
2023-04-15 22:29:13 -0500

    4/14 meeting

--- nn1d/fitting1d.py
+++ nn1d/fitting1d.py
@@ -1,34 +1,74 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
-
-# def ballHeight(t):
-#     STARTING_HEIGHT = 10
-#     GRAVITATIONAL_ACCELERATION = 9.81
-#     return STARTING_HEIGHT - 0.5 * GRAVITATIONAL_ACCELERATION * t**2 
-
+from singleLayerNetwork1D import SingleLayerNetwork1D
 
 def ballHeight(t):
     STARTING_HEIGHT = 10
     GRAVITATIONAL_ACCELERATION = 9.81
-    return STARTING_HEIGHT - 0.5 * GRAVITATIONAL_ACCELERATION * t**2 + np.sin(20*t)
-
-
-T_MIN = 0.0
-T_MAX = 1.0
-NUMBER_OF_POINTS = 20
-NOISE_STDEV = 0.5 
-t_measured = np.linspace(T_MIN, T_MAX, NUMBER_OF_POINTS)
-h_ball = ballHeight(t_measured)
-h_noise = NOISE_STDEV * np.random.randn(NUMBER_OF_POINTS)
-h_measured = h_ball + h_noise
-
-plt.figure()
-plt.plot(t_measured, h_ball, 'o')
-plt.plot(t_measured, h_measured, 'o')
-
-plt.show()
+    return STARTING_HEIGHT - 0.5 * GRAVITATIONAL_ACCELERATION * t**2 
 
 def generateBallHeightData(t_min, t_max, n_points, noise_stdev):
     t_measured = 0
     h_measured = 0
     return t_measured, h_measured
+
+# def ballHeight(t):
+#     STARTING_HEIGHT = 10
+#     GRAVITATIONAL_ACCELERATION = 9.81
+#     return STARTING_HEIGHT - 0.5 * GRAVITATIONAL_ACCELERATION * t**2 + np.sin(20*t)
+
+
+T_MIN = 0.0
+T_MAX = 1.0
+NUMBER_OF_POINTS = 5
+NOISE_STDEV = 1.0
+t_measured = np.linspace(T_MIN, T_MAX, NUMBER_OF_POINTS)
+h_ball = ballHeight(t_measured)
+h_noise = NOISE_STDEV * np.random.randn(NUMBER_OF_POINTS)
+h_measured = h_ball + h_noise
+
+WIDTH = 100
+weight_input = np.random.randn(WIDTH)
+bias_input = np.random.randn(WIDTH)
+weight_output = np.random.randn(WIDTH)
+
+# Construct NN
+myNeuralNetwork = SingleLayerNetwork1D(weight_input, bias_input, weight_output)
+
+# Make prediction with NN (before training)
+h_predict_before = np.zeros(len(t_measured))
+for i in range(len(t_measured)):
+    h_predict_before[i] = myNeuralNetwork.forward(t_measured[i])
+
+# Steepest descent training
+STEPSIZE = 0.01 
+N_ITER = 50000
+for i in range(N_ITER):
+    input_weight_gradient, bias_gradient, output_weight_gradient = myNeuralNetwork.gradient(t_measured, h_measured)
+    myNeuralNetwork.weight_input -= STEPSIZE * input_weight_gradient
+    myNeuralNetwork.weight_output -= STEPSIZE * output_weight_gradient
+    myNeuralNetwork.bias_input -= STEPSIZE * bias_gradient
+    if i % 100 == 0:
+        print("Iteration ", i)
+
+
+# Make prediction with NN (after training)
+h_predict_after = np.zeros(len(t_measured))
+for i in range(len(t_measured)):
+    h_predict_after[i] = myNeuralNetwork.forward(t_measured[i])
+
+t_fine = np.linspace(T_MIN, T_MAX, 100)
+h_predict_fine = np.zeros(len(t_fine))
+for i in range(len(t_fine)):
+    h_predict_fine[i] = myNeuralNetwork.forward(t_fine[i])
+
+
+plt.figure()
+plt.plot(t_measured, h_ball, 'o', label="True")
+plt.plot(t_measured, h_measured, 'o', label="Noisy data")
+plt.plot(t_measured, h_predict_before, 'o', label="NN prediction before training")
+plt.plot(t_measured, h_predict_after, 'o', label="NN prediction after training")
+plt.plot(t_fine, h_predict_fine, '-', label="NN prediction after training")
+plt.legend()
+plt.show()
+
--- nn1d/singleLayerNetwork1D.py
+++ nn1d/singleLayerNetwork1D.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class SingleLayerNetwork1D:
-    def __init__(self, weight_input, bias_input, weights_output, activation="sigmoid"):
+    def __init__(self, weight_input, bias_input, weight_output, activation="sigmoid"):
         self.weight_input = weight_input
         self.bias_input = bias_input
-        self.weights_output = weights_output
+        self.weight_output = weight_output
         self.activation = activation 
 
     def forward(self, x):
@@ -14,23 +14,49 @@ class SingleLayerNetwork1D:
         """
         if self.activation == "sigmoid":
             a = sigmoid(self.weight_input * x + self.bias_input)
-            y = np.inner(self.weights_output, a)
+            y = np.inner(self.weight_output, a)
             return y 
 
         if self.activation == "relu":
             a = relu(self.weight_input * x + self.bias_input)
-            y = np.inner(self.weights_output, a)
+            y = np.inner(self.weight_output, a)
             return y 
         else:
             a = self.weight_input * x + self.bias_input
-            y = np.inner(self.weights_output, a)
+            y = np.inner(self.weight_output, a)
             return y 
     
-    def update(self, w_new, b_new):
-        pass
+    def gradient(self, x_array, y_array):
+        assert self.activation == "sigmoid" 
+        assert len(x_array) == len(y_array)
+
+        input_weight_gradient = np.zeros(len(self.weight_input))
+        output_weight_gradient = np.zeros(len(self.weight_output))
+        bias_gradient = np.zeros(len(self.bias_input))
+
+        n_data = len(x_array)
+        for i in range(n_data):
+            misfit = y_array[i] - self.forward(x_array[i])
+
+            output_weight_gradient -= misfit \
+                * sigmoid(self.weight_input * x_array[i] + self.bias_input) * 2 / n_data
+            
+            input_weight_gradient -= misfit \
+                * sigmoid_derivative(self.weight_input * x_array[i] + self.bias_input) \
+                * x_array[i] * 2 / n_data
+            
+            bias_gradient -= misfit \
+                * sigmoid_derivative(self.weight_input * x_array[i] + self.bias_input) \
+                * 2 / n_data
+
+        return input_weight_gradient, bias_gradient, output_weight_gradient
+
 
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    return np.exp(-x)/(1 + np.exp(-x))**2
 
 def relu(x):
     return np.maximum(x, 0)
